9830af7a59c8dbf3d5fd402c36b047889bdb5681
Binoy Saha <binoy.saha01@northsouth.edu>
2022-06-09 23:44:29 +0600

    4

---
+++ javascript.js
@@ -0,0 +1,18 @@
+print=getMiddle('abcd');
+console.log(print);
+
+
+
+
+function getMiddle(s)
+{
+  if(s.length%2==0){
+      
+
+return  s[ (s.length/2)-1 ]+ s[s.length/2];
+
+  }
+  else{
+    return s[ (s.length/2)-0.5 ] ;
+    }
+}
